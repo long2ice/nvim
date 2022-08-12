7d6fc360d14bca7ba0c4c802e04b6ae39e3ac74c
Jinlong Peng <jinlong.peng@merico.dev>
2022-08-12 23:13:30 +0800

    update README

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -1,12 +1,3 @@
-local has_words_before = function()
-	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
-
-local feedkey = function(key, mode)
-	vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
-end
-
 local opts = { noremap = true, silent = true }
 local map = vim.api.nvim_set_keymap
 
@@ -44,6 +35,16 @@ pluginKeys.maplsp = function(mapbuf)
 end
 
 -- nvim-cmp 自动补全
+local has_words_before = function()
+	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+local feedkey = function(key, mode)
+	vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+end
+
+
 pluginKeys.cmp = function(cmp)
 	return {
 		["<Tab>"] = cmp.mapping(function(fallback)
